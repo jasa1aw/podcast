22fd6fdd6c2a77074aa06fd14d3588e0d4d22b16
jasa1aw <erakrut05@gmail.com>
2025-02-02 23:01:32 +0500

    create podcast page

---
+++ components/GeneratePodcast.tsx
@@ -0,0 +1,7 @@
+
+
+export default function GeneratePodcast() {
+	return (
+		<div>GeneratePodcast</div>
+	)
+}
---
+++ components/GenerateThumbnail.tsx
@@ -0,0 +1,6 @@
+
+export default function GenerateThumbnail() {
+	return (
+		<div>GenerateThumbnail</div>
+	)
+}
